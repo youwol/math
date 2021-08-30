8c5e064165187107703e2807d61cc5844195e5b9
Xaliphostes <fmaerten@youwol.com>
2021-08-30 09:41:03 +0200

    added inversion of itemized matrices

--- src/lib/dataframe/inv.ts
+++ src/lib/dataframe/inv.ts
@@ -9,31 +9,33 @@ export const inv = (s: Serie, throwOnDegenerate = false): Serie => {
     if (s.itemSize !== 6 && s.itemSize !== 9) throw new Error ('itemSize must be 6 or 9')
 
     if (s.itemSize===9) {
-        return s.map( (me, index) => {    
-            let n11 = me[0], n12 = me[1], n13 = me[2],
-                n21 = me[3], n22 = me[4], n23 = me[5],
-                n31 = me[6], n32 = me[7], n33 = me[8]
-            let t11 = n33 * n22 - n32 * n23,
-                t12 = n32 * n13 - n33 * n12,
-                t13 = n23 * n12 - n22 * n13,
-                det = n11 * t11 + n21 * t12 + n31 * t13
+        // https://stackoverflow.com/a/18504573
+        return s.map( (m, index) => {
+            const det = m[0] * (m[4] * m[8] - m[7] * m[5]) -
+                m[1] * (m[3] * m[8] - m[5] * m[6]) +
+                m[2] * (m[3] * m[7] - m[4] * m[6])
 
-            if (det === 0) {
+            if (det===0) {
                 let msg = `matrix at index${index} has a determinant = 0`
                 if (throwOnDegenerate === true) {
                     throw new Error(msg)
                 } else {
                     console.warn(msg)
                 }
-                return [1,0,0,0,1,0,0,0,1]
+                return [0,0,0,0,0,0,0,0,0]
             }
-    
-            let detInv = 1 / det
-            return [
-                t11 * detInv, (n31 * n23 - n33 * n21) * detInv, (n32 * n21 - n31 * n22) * detInv,
-                t12 * detInv, (n33 * n11 - n31 * n13) * detInv, (n31 * n12 - n32 * n11) * detInv,
-                t13 * detInv, (n21 * n13 - n23 * n11) * detInv, (n22 * n11 - n21 * n12) * detInv
-            ]
+            const invdet = 1 / det
+            const minv  = []
+            minv[0] = (m[4] * m[8] - m[7] * m[5]) * invdet
+            minv[1] = (m[2] * m[7] - m[1] * m[8]) * invdet
+            minv[2] = (m[1] * m[5] - m[2] * m[4]) * invdet
+            minv[3] = (m[5] * m[6] - m[3] * m[8]) * invdet
+            minv[4] = (m[0] * m[8] - m[2] * m[6]) * invdet
+            minv[5] = (m[3] * m[2] - m[0] * m[5]) * invdet
+            minv[6] = (m[3] * m[7] - m[6] * m[4]) * invdet
+            minv[7] = (m[6] * m[1] - m[0] * m[7]) * invdet
+            minv[8] = (m[0] * m[4] - m[3] * m[1]) * invdet
+            return minv
         })
     }
     else {
@@ -53,7 +55,7 @@ export const inv = (s: Serie, throwOnDegenerate = false): Serie => {
                 } else {
                     console.warn(msg)
                 }
-                return [1,0,0,1,0,1]
+                return [0,0,0,0,0,0]
             }
     
             let detInv = 1 / det
